4cff399aa7224e0d3d81b64dcd64852b5d55c3a4
Su Ngo <quocsungo2000@gmail.com>
2025-05-27 17:06:24 +0700

    user authentication feature

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {NavbarIndex} from './component/Navbar.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <NavbarIndex />
   </StrictMode>,
 )
